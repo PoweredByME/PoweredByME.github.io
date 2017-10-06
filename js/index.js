daff9fa86dde9b65d025f8cf452ec1ada4872b7e
PoweredByME <saadkwi12@hotmail.com>
2017-10-06 18:45:26 +0500

    pubnub

--- js/index.js
+++ js/index.js
@@ -1,35 +1,77 @@
-var peer, peerConn;
+var video_out;
 
 function indexJS_onWindowLoad(){
     console.log("Hello World");
-    setupPeerJS();
+    video_out = document.getElementById("vid-box");
+    //setupWebRTC();
 }
 
-function setupPeerJS(){
-    console.log("Setting up PeerJS");
-    peer = new Peer({key : '2wa2nwzh865pzaor',  debug : 3, ssl: false});
-    
-    peer.on('open', function(id){
-        $(".p-span-id-class").empty();
-        $(".p-span-id-class").append(id);
-    }); // end peer.on('open', function(id){});
-    
-    peer.on('connection', function(conn){
-       conn.on('data', function(data){
-           console.log("recved data = " + data);
-           $(".recv-span-id-class").empty();
-           $(".recv-span-id-class").append(data);
-       });
-    });
-    
-} // end setupPeerJS
 
-function connectToRemotePeer(peerId){
-    console.log("peerId = " + peerId)
-    peerConn =  peer.connect(peerId);
-    peerConn.on('open', function(){
-        console.log("connection Opened");
-        peerConn.send('hi!');
-    });
+function login(form) {
+    console.log("login");
+	var phone = window.phone = PHONE({
+	    number        : form.username.value || "Anonymous", // listen on username line else Anonymous
+	    publish_key   : 'pub-c-c904a156-f71f-4800-a8ac-500c05061cc5',
+	    subscribe_key : 'sub-c-eeeb0938-aa9b-11e7-9eb5-def16b84ebc1',
+	});	
+	phone.ready(function(){ form.username.style.background="#55ff5b"; });
+	phone.receive(function(session){
+	    session.connected(function(session) { video_out.appendChild(session.video); });
+	    session.ended(function(session) { video_out.innerHTML=''; });
+	});
+	return false; 	// So the form does not submit.
 }
 
+
+function makeCall(form){
+	if (!window.phone) alert("Login First!");
+	else phone.dial(form.number.value);
+	return false;
+}
+
+
+/*function setupWebRTC(){
+    skylink = new Skylink();
+    
+    skylink.on('peerJoined', function(peerId, peerInfo, isSelf) {
+        console.log("Peer joined = " + peerInfo);
+        if(isSelf) return; // We already have a video element for our video and don't need to create a new one.
+        var vid = document.createElement('video');
+        vid.autoplay = true;
+        vid.muted = true; // Added to avoid feedback when testing locally
+        vid.id = peerId;
+        document.body.appendChild(vid);
+    });
+    
+    skylink.on('incomingStream', function(peerId, stream, isSelf) {
+        if(isSelf) return;
+        var vid = document.getElementById(peerId);
+        attachMediaStream(vid, stream);
+    });
+    
+    skylink.on('peerLeft', function(peerId, peerInfo, isSelf) {
+        var vid = document.getElementById(peerId);
+        document.body.removeChild(vid);
+    });
+    
+    skylink.on('mediaAccessSuccess', function(stream) {
+        var vid = document.getElementById('myvideo');
+        attachMediaStream(vid, stream);
+    });
+    
+    skylink.init({
+        apiKey: '3e850fd6-c814-4610-b1d1-81f0b1dd1841',
+        defaultRoom: 'room12'
+        }, function() {
+            skylink.joinRoom({
+                audio: true,
+                video: true
+        });
+    });
+    
+    SkylinkLogs.printAllLogs();
+    
+} // end setupWebRTC
+
+
+*/
