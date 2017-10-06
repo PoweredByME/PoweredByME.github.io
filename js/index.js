b5921ba9bc48d4c2bb404e2d7c9f4ec16851fcb6
PoweredByME <saadkwi12@hotmail.com>
2017-10-06 18:48:38 +0500

    m

--- js/index.js
+++ js/index.js
@@ -13,13 +13,14 @@ function login(form) {
 	    number        : form.username.value || "Anonymous", // listen on username line else Anonymous
 	    publish_key   : 'pub-c-c904a156-f71f-4800-a8ac-500c05061cc5',
 	    subscribe_key : 'sub-c-eeeb0938-aa9b-11e7-9eb5-def16b84ebc1',
+        ssl : true
 	});	
 	phone.ready(function(){ form.username.style.background="#55ff5b"; });
 	phone.receive(function(session){
 	    session.connected(function(session) { video_out.appendChild(session.video); });
 	    session.ended(function(session) { video_out.innerHTML=''; });
 	});
-	return false; 	// So the form does not submit.
+    return false; 	// So the form does not submit.
 }
 
 
