b6c8fea2ddf797ff47c4361a307b2c17f190d81c
PoweredByME <saadkwi12@hotmail.com>
2018-03-04 03:25:06 +0500

    added nazdeeq for somethings

---
+++ nazdeeq/js/index.js
@@ -0,0 +1,20 @@
+/*
+    This file contains css related to 
+    index.html, signup.html, login.html
+*/
+
+// This function gets called when the window gets loaded
+function windowOnLoad() {
+    $(".the-body").removeClass("body-no-scroll");
+    $(".button-collapse").sideNav();
+}
+
+// This function gets called when the window gets resized
+function windowOnResize() {
+
+}
+
+// This function gets called when the window gets scrolled
+function windowOnScroll() {
+
+}
