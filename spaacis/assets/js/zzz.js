dd63a192d15788ded63e209c4cab25f0e741ee35
Saad Ahmad <deburgers123@gmail.com>
2019-05-13 00:28:38 +0500

    updated spaacis

---
+++ spaacis/assets/js/zzz.js
@@ -0,0 +1,62 @@
+$("#search-menu-toggle-btn").on('click', function(){
+    $("#search-menu").toggleClass('hidden');
+});
+
+
+$(function() {
+           
+$(".js-modal-btn").modalVideo()
+    
+    $(".js-modal-div").modalVideo()
+    
+    
+    
+    $('button[name="daterange-1"]').daterangepicker({
+            opens: 'left',
+                timePicker: true,
+        }, function(start, end, label) {
+      
+      
+        console.log(start);
+                console.log(end);
+                console.log(label);
+    
+  });
+     
+     
+      $('div[name="daterange-2"]').daterangepicker({
+            opens: 'left'
+        }, function(start, end, label) {
+      
+      
+        $("#mao-data-pick-start-2").html(start.format('DD/MM/YYYY'));
+      $("#mao-data-pick-end-2").html(end.format('DD/MM/YYYY'));
+    
+  });
+     
+     
+      $('div[name="daterange-3"]').daterangepicker({
+            opens: 'left'
+        }, function(start, end, label) {
+      
+      
+        $("#mao-data-pick-start-3").html(start.format('DD/MM/YYYY'));
+      $("#mao-data-pick-end-3").html(end.format('DD/MM/YYYY'));
+    
+  });
+     
+     
+     
+      $('div[name="daterange-4"]').daterangepicker({
+            opens: 'left'
+        }, function(start, end, label) {
+      
+      
+        $("#mao-data-pick-start-4").html(start.format('DD/MM/YYYY'));
+      $("#mao-data-pick-end-4").html(end.format('DD/MM/YYYY'));
+    
+  });
+     
+     
+     
+});
